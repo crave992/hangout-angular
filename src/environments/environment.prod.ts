81b348c80a0782f5bfdf63efaebbbea24e94c814
Adrian Jairus Valbuena <adrianwebpaypal@gmail.com>
2022-08-26 22:33:10 +0800

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'hangout-calleuno',
+    appId: '1:412503490475:web:341af1ae548d64b7791cee',
+    storageBucket: 'hangout-calleuno.appspot.com',
+    apiKey: 'AIzaSyDrWO3C791fdCT0_fmbxu4Q2au-xy7PygQ',
+    authDomain: 'hangout-calleuno.firebaseapp.com',
+    messagingSenderId: '412503490475',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'hangout-calleuno',
+    appId: '1:412503490475:web:341af1ae548d64b7791cee',
+    storageBucket: 'hangout-calleuno.appspot.com',
+    apiKey: 'AIzaSyDrWO3C791fdCT0_fmbxu4Q2au-xy7PygQ',
+    authDomain: 'hangout-calleuno.firebaseapp.com',
+    messagingSenderId: '412503490475',
+  },
   production: false
 };
 
